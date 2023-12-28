8b32599cc3701a71b2dcd51dcc3c501fb7f1bbc6
Oleksandr Igumnov <igumssukr@gmail.com>
2023-12-28 20:23:14 +0200

    Configured swagger, backend and frontend Dockerfile.dev and docker-compose.yml

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Course Work')
+    .setDescription('The Course Work API description')
+    .setVersion('3.0')
+    .build();
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.use(cookieParser());
+  app.setGlobalPrefix('api');
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(process.env.BACKEND_PORT);
 }
 bootstrap();
