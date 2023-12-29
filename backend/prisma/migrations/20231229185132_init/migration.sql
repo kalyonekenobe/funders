36bed65d221cfa5061b3b31ee0725b909a22d606
Oleksandr Igumnov <igumssukr@gmail.com>
2023-12-29 20:53:39 +0200

    Created prisma database schema and connected to database container. Configured migrations

---
+++ backend/prisma/migrations/20231229185132_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User2" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User2_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User3" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User3_pkey" PRIMARY KEY ("id")
+);
---
+++ backend/prisma/migrations/20231229185149_init/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User2` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User3` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- DropTable
+DROP TABLE "User2";
+
+-- DropTable
+DROP TABLE "User3";
