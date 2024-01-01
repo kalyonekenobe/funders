f8c835739a8b9b555fddaf3e1544502c67a41ff8
Oleksandr Igumnov <igumssukr@gmail.com>
2024-01-01 10:13:37 +0200

    Configured UserRegistrationMethod module

--- backend/src/user-registration-method/user-registration-method.module.ts
+++ backend/src/user-registration-method/user-registration-method.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/core/prisma/prisma.module';
+import { UserRegistrationMethodController } from './user-registration-method.controller';
+import { UserRegistrationMethodService } from './user-registration-method.service';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  controllers: [UserRegistrationMethodController],
+  providers: [UserRegistrationMethodService],
+  exports: [UserRegistrationMethodService],
+})
 export class UserRegistrationMethodModule {}
