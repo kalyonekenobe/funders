604b27c99d322164b71102d7c15a5aa1ebfdcf44
Oleksandr Igumnov <igumssukr@gmail.com>
2024-01-02 14:11:57 +0200

    Added field validation for UserRegistrationMethod entity and dto

--- backend/src/user-registration-method/dto/update-user-registration-method.dto.ts
+++ backend/src/user-registration-method/dto/update-user-registration-method.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { UserRegistrationMethod } from '@prisma/client';
+import { IsNotEmpty, Matches, MaxLength } from 'class-validator';
 
 export class UpdateUserRegistrationMethodDto implements UserRegistrationMethod {
   @ApiProperty({
@@ -7,5 +8,8 @@ export class UpdateUserRegistrationMethodDto implements UserRegistrationMethod {
     examples: ['Google', 'Facebook', 'Microsoft', 'Apple', 'LinkedIn'],
     default: 'Google',
   })
+  @Matches(/^[a-zA-Z_0-9]+$/)
+  @MaxLength(50)
+  @IsNotEmpty()
   name: string;
 }
