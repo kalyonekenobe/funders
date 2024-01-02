e84c84966ffaedce37642406e038f4483257fe76
Oleksandr Igumnov <igumssukr@gmail.com>
2024-01-02 16:37:35 +0200

    Added UserRole entity and dto

---
+++ backend/src/user-role/dto/update-user-role.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { UserRole } from '@prisma/client';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsNumber, Matches, Max, MaxLength } from 'class-validator';
+
+export class UpdateUserRoleDto implements UserRole {
+  @ApiProperty({
+    description: 'Name of the user role',
+    examples: ['Default', 'Volunteer', 'Administrator'],
+    default: 'Volunteer',
+  })
+  @Matches(/^[a-zA-Z_0-9]+$/)
+  @MaxLength(50)
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: 'The total value of user role permissions',
+    examples: [255, 15, 127, 31],
+    default: 255,
+  })
+  @IsNumber()
+  @Transform(number => BigInt(number.value))
+  @Max(2 ** 64 - 1)
+  permissions: bigint;
+}
