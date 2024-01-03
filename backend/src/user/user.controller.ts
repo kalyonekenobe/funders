724f264334985ca48b1f8f606cc311f7244c58a4
Oleksandr Igumnov <igumssukr@gmail.com>
2024-01-03 09:15:57 +0200

    Created intitial files for User model

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -6,9 +6,4 @@ import { UserService } from './user.service';
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -4,8 +4,4 @@ import { PrismaService } from 'src/core/prisma/prisma.service';
 @Injectable()
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
-
-  async findAll() {
-    return this.prismaService.user.findMany();
-  }
 }
