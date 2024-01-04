5a433af293ba913bc3720350ffb9b15388031690
Oleksandr Igumnov <igumssukr@gmail.com>
2024-01-04 10:15:50 +0200

    Created unit and e2e tests for User service and controller

---
+++ backend/test/user/user.controller.spec.ts
@@ -0,0 +1,168 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { MockDataStorage, mockUserService } from './user.mock';
+import { UserController } from 'src/user/user.controller';
+import { UserService } from 'src/user/user.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+
+describe('UserController', () => {
+  let controller: UserController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UserController>(UserController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should create a list of new users', async () => {
+    MockDataStorage.setDefaultItems();
+
+    const initialItems = [...MockDataStorage.items()];
+    for (const item of MockDataStorage.createUserDtoList) {
+      const received = await controller.create(item);
+      const expected = { ...received, ...item };
+      expect({ ...received, password: item.password }).toEqual(expected);
+      initialItems.push(received);
+    }
+
+    expect(MockDataStorage.items()).toEqual(initialItems);
+
+    MockDataStorage.setDefaultItems();
+    expect(mockUserService.create).toHaveBeenCalled();
+  });
+
+  it('should not create a new user because it already exists', () => {
+    MockDataStorage.setDefaultItems();
+
+    const initialItems = [...MockDataStorage.items()];
+    expect(() =>
+      controller.create({
+        ...MockDataStorage.items()[0],
+        email: 'johndoe@gmail.com',
+        password: '123',
+      } as CreateUserDto),
+    ).toThrow();
+    expect(MockDataStorage.items()).toEqual(initialItems);
+
+    MockDataStorage.setDefaultItems();
+    expect(mockUserService.create).toHaveBeenCalled();
+  });
+
+  it('should find all existing users', async () => {
+    MockDataStorage.setDefaultItems();
+
+    const initialItems = [...MockDataStorage.items()];
+    expect(await controller.findAll()).toEqual(initialItems);
+
+    MockDataStorage.setDefaultItems();
+    expect(mockUserService.findAll).toHaveBeenCalled();
+  });
+
+  it('should find users list by user id', async () => {
+    MockDataStorage.setDefaultItems();
+
+    const initialItems = [...MockDataStorage.items()];
+    for (const item of MockDataStorage.items()) {
+      expect(await controller.findByPk(item.id)).toEqual(item);
+    }
+
+    expect(MockDataStorage.items()).toEqual(initialItems);
+
+    MockDataStorage.setDefaultItems();
+    expect(mockUserService.findByPk).toHaveBeenCalled();
+  });
+
+  it('should not find user with provided id because it does not exist', () => {
+    MockDataStorage.setDefaultItems();
+
+    const initialItems = [...MockDataStorage.items()];
+
+    expect(() => controller.findByPk('')).toThrow();
+    expect(MockDataStorage.items()).toEqual(initialItems);
+
+    MockDataStorage.setDefaultItems();
+    expect(mockUserService.findByPk).toHaveBeenCalled();
+  });
+
+  it('should update a list of existing users by provided ids', async () => {
+    MockDataStorage.setDefaultItems();
+
+    const initialItems = [...MockDataStorage.items()];
+    const updatedItems: any[] = [];
+    for (const item of MockDataStorage.updateUserDtoList) {
+      const received = await controller.update(item.id, item.data);
+      const expected = Object.assign(
+        {},
+        initialItems.find(x => x.id === item.id),
+        item.data,
+      );
+      expect({ ...received, password: expected.password }).toEqual(expected);
+      updatedItems.push(received);
+    }
+
+    expect(MockDataStorage.items()).toEqual(
+      initialItems.map(item => {
+        const updated = updatedItems.find(x => x.id === item.id);
+
+        return updated ? updated : item;
+      }),
+    );
+
+    MockDataStorage.setDefaultItems();
+    expect(mockUserService.update).toHaveBeenCalled();
+  });
+
+  it('should not update a user with provided id because it does not exist', () => {
+    MockDataStorage.setDefaultItems();
+
+    const initialItems = [...MockDataStorage.items()];
+    expect(() => controller.update('', { ...MockDataStorage.updateUserDtoList[0].data })).toThrow();
+    expect(MockDataStorage.items()).toEqual(initialItems);
+
+    MockDataStorage.setDefaultItems();
+    expect(mockUserService.update).toHaveBeenCalled();
+  });
+
+  it('should remove a list of existing users by provided ids', async () => {
+    MockDataStorage.setDefaultItems();
+
+    const initialItems = [...MockDataStorage.items()];
+    for (const item of MockDataStorage.removeUserDtoList) {
+      expect(await controller.remove(item.id)).toEqual({
+        ...item,
+        registeredAt: expect.any(Date),
+      });
+    }
+
+    expect(MockDataStorage.items()).toEqual(
+      initialItems.filter(item => {
+        return !MockDataStorage.removeUserDtoList.find(x => x.id === item.id);
+      }),
+    );
+
+    MockDataStorage.setDefaultItems();
+    expect(mockUserService.remove).toHaveBeenCalled();
+  });
+
+  it('should not remove a user with provided id because it does not exist', () => {
+    MockDataStorage.setDefaultItems();
+
+    const initialItems = [...MockDataStorage.items()];
+    expect(() => controller.remove('')).toThrow();
+    expect(MockDataStorage.items()).toEqual(initialItems);
+
+    MockDataStorage.setDefaultItems();
+    expect(mockUserService.remove).toHaveBeenCalled();
+  });
+});
---
+++ backend/test/user/user.mock.ts
@@ -0,0 +1,309 @@
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UpdateUserDto } from 'src/user/dto/update-user.dto';
+import { UserPublicEntity } from 'src/user/entities/user-public.entity';
+import { UserEntity } from 'src/user/entities/user.entity';
+
+// Mock data storage
+export class MockDataStorage {
+  static #data: UserPublicEntity[] = [];
+  static #defaultData: UserPublicEntity[] = [
+    {
+      id: 'b7af9cd4-5533-4737-862b-78bce985c987',
+      registrationMethod: 'LinkedIn',
+      role: 'Default',
+      firstName: 'John',
+      lastName: 'Doe',
+      birthDate: new Date('1996-07-04'),
+      email: 'johndoe@gmail.com',
+      phone: null,
+      bio: null,
+      avatar: null,
+      refreshToken: null,
+      registeredAt: new Date(),
+    },
+    {
+      id: '989d32c2-abd4-43d3-a420-ee175ae16b98',
+      registrationMethod: 'LinkedIn',
+      role: 'Administrator',
+      firstName: 'Alex',
+      lastName: 'Igumnov',
+      birthDate: new Date('2004-09-03'),
+      email: 'alexigumnov@gmail.com',
+      phone: null,
+      bio: null,
+      avatar: null,
+      refreshToken: null,
+      registeredAt: new Date(),
+    },
+    {
+      id: '34f79b86-2151-4483-838f-0a0ed586a621',
+      registrationMethod: 'Instagram',
+      role: 'Volunteer',
+      firstName: 'Samantha',
+      lastName: 'Jones',
+      birthDate: new Date('1999-03-17'),
+      email: 'samanthajones@gmail.com',
+      phone: null,
+      bio: null,
+      avatar: null,
+      refreshToken: null,
+      registeredAt: new Date(),
+    },
+    {
+      id: '26e6fdab-bbf8-4500-b78b-56378421b230',
+      registrationMethod: 'Twitter',
+      role: 'Volunteer',
+      firstName: 'Vova',
+      lastName: 'Havryliuk',
+      birthDate: new Date('2003-12-21'),
+      email: 'vovahavryliuk@gmail.com',
+      phone: null,
+      bio: null,
+      avatar: null,
+      refreshToken: null,
+      registeredAt: new Date(),
+    },
+    {
+      id: '23fbed56-1bb9-40a0-8977-2dd0f0c6c31f',
+      registrationMethod: 'Atlassian',
+      role: 'Default',
+      firstName: 'Helen',
+      lastName: 'Effenberg',
+      birthDate: new Date('2004-01-30'),
+      email: 'heleneffenberg@gmail.com',
+      phone: null,
+      bio: null,
+      avatar: null,
+      refreshToken: null,
+      registeredAt: new Date(),
+    },
+  ];
+
+  static createUserDtoList: CreateUserDto[] = [
+    {
+      registrationMethod: 'Atlassian',
+      role: 'Default',
+      firstName: 'Oleksandr',
+      lastName: 'Shved',
+      birthDate: new Date('1974-11-14'),
+      email: 'oleksandrshved@gmail.com',
+      password: 'password',
+    },
+    {
+      registrationMethod: 'Atlassian',
+      role: 'Default',
+      firstName: 'Margaret',
+      lastName: 'Johansenn',
+      birthDate: new Date('2007-04-06'),
+      email: 'margaretjohansenn@gmail.com',
+      password: 'password',
+    },
+  ];
+
+  static updateUserDtoList: {
+    id: string;
+    data: UpdateUserDto;
+  }[] = [
+    { id: '23fbed56-1bb9-40a0-8977-2dd0f0c6c31f', data: { firstName: 'Alex' } },
+    { id: '26e6fdab-bbf8-4500-b78b-56378421b230', data: { lastName: 'Boba' } },
+    { id: '989d32c2-abd4-43d3-a420-ee175ae16b98', data: { password: 'jasnfjasbfbasfbsahfbhas' } },
+  ];
+
+  static removeUserDtoList: UserPublicEntity[] = [
+    {
+      id: '34f79b86-2151-4483-838f-0a0ed586a621',
+      registrationMethod: 'Instagram',
+      role: 'Volunteer',
+      firstName: 'Samantha',
+      lastName: 'Jones',
+      birthDate: new Date('1999-03-17'),
+      email: 'samanthajones@gmail.com',
+      phone: null,
+      bio: null,
+      avatar: null,
+      refreshToken: null,
+      registeredAt: new Date(),
+    },
+    {
+      id: '26e6fdab-bbf8-4500-b78b-56378421b230',
+      registrationMethod: 'Twitter',
+      role: 'Volunteer',
+      firstName: 'Vova',
+      lastName: 'Havryliuk',
+      birthDate: new Date('2003-12-21'),
+      email: 'vovahavryliuk@gmail.com',
+      phone: null,
+      bio: null,
+      avatar: null,
+      refreshToken: null,
+      registeredAt: new Date(),
+    },
+  ];
+
+  static items() {
+    return MockDataStorage.#data;
+  }
+
+  static setItems(data: UserPublicEntity[]) {
+    MockDataStorage.#data = [...data];
+  }
+
+  static setDefaultItems() {
+    MockDataStorage.#data = [...MockDataStorage.#defaultData];
+  }
+}
+
+export const mockUserService = {
+  findAll: jest.fn().mockImplementation(() => Promise.resolve(MockDataStorage.items())),
+  findByPk: jest.fn().mockImplementation((id: string) => {
+    const dto = MockDataStorage.items().find(item => item.id === id);
+
+    if (!dto) {
+      throw new Error('User with this id does not exist!');
+    }
+
+    return Promise.resolve(dto);
+  }),
+  create: jest.fn().mockImplementation((dto: CreateUserDto): Promise<UserPublicEntity> => {
+    const exists = MockDataStorage.items().find(item => item.email === dto.email);
+
+    if (exists) {
+      throw new Error('User with this email already exists!');
+    }
+
+    const created = {
+      ...dto,
+      bio: null,
+      avatar: null,
+      id: '',
+      phone: null,
+      refreshToken: null,
+      registeredAt: new Date(),
+    } as Partial<UserEntity>;
+
+    delete created.password;
+
+    MockDataStorage.items().push(created as UserPublicEntity);
+
+    return Promise.resolve(created as UserPublicEntity);
+  }),
+  update: jest
+    .fn()
+    .mockImplementation((id: string, dto: UpdateUserDto): Promise<UserPublicEntity> => {
+      let exists = MockDataStorage.items().find(item => item.id === id);
+
+      if (!exists) {
+        throw new Error('User with this id does not exist!');
+      }
+
+      const updated = { ...exists, ...dto };
+
+      MockDataStorage.setItems(
+        MockDataStorage.items().map(item =>
+          item.id === id ? (updated as UserPublicEntity) : item,
+        ),
+      );
+
+      return Promise.resolve(updated as UserPublicEntity);
+    }),
+  remove: jest.fn().mockImplementation((id: string): Promise<UserPublicEntity> => {
+    const dto = MockDataStorage.items().find(item => item.id === id);
+
+    if (!dto) {
+      throw new Error('User with this id does not exist!');
+    }
+
+    MockDataStorage.setItems(MockDataStorage.items().filter(item => item.id !== id));
+
+    return Promise.resolve(dto);
+  }),
+};
+
+export const mockUserRepository = {
+  user: {
+    findMany: jest.fn().mockImplementation(() => MockDataStorage.items()),
+    findUniqueOrThrow: jest.fn().mockImplementation((data: { where: { id: string } }) => {
+      const dto = MockDataStorage.items().find(item => item.id === data.where.id);
+
+      if (!dto) {
+        throw new PrismaClientKnownRequestError('User with this id does not exist!', {
+          code: 'P2001',
+          clientVersion: '',
+        });
+      }
+
+      return Promise.resolve(dto);
+    }),
+    create: jest
+      .fn()
+      .mockImplementation((dto: { data: CreateUserDto }): Promise<UserPublicEntity> => {
+        const exists = MockDataStorage.items().find(item => item.email === dto.data.email);
+
+        if (exists) {
+          throw new PrismaClientValidationError('User with this email already exists!', {
+            clientVersion: '',
+          });
+        }
+
+        const created = {
+          ...dto.data,
+          bio: null,
+          avatar: null,
+          id: '',
+          phone: null,
+          refreshToken: null,
+          registeredAt: new Date(),
+        } as Partial<UserEntity>;
+
+        delete created.password;
+
+        MockDataStorage.items().push(created as UserPublicEntity);
+
+        return Promise.resolve(created as UserPublicEntity);
+      }),
+    update: jest
+      .fn()
+      .mockImplementation(
+        (dto: { where: { id: string }; data: UpdateUserDto }): Promise<UserPublicEntity> => {
+          let exists = MockDataStorage.items().find(item => item.id === dto.where.id);
+
+          if (!exists) {
+            throw new PrismaClientKnownRequestError('User with this id does not exist!', {
+              code: 'P2001',
+              clientVersion: '',
+            });
+          }
+
+          const updated = { ...exists, ...dto.data };
+
+          MockDataStorage.setItems(
+            MockDataStorage.items().map(item =>
+              item.id === dto.where.id ? (updated as UserPublicEntity) : item,
+            ),
+          );
+
+          return Promise.resolve(updated as UserPublicEntity);
+        },
+      ),
+    delete: jest
+      .fn()
+      .mockImplementation((data: { where: { id: string } }): Promise<UserPublicEntity> => {
+        const dto = MockDataStorage.items().find(item => item.id === data.where.id);
+
+        if (!dto) {
+          throw new PrismaClientKnownRequestError('User this id does not exist!', {
+            code: 'P2001',
+            clientVersion: '',
+          });
+        }
+
+        MockDataStorage.setItems(MockDataStorage.items().filter(item => item.id !== data.where.id));
+
+        return Promise.resolve(dto);
+      }),
+  },
+};
