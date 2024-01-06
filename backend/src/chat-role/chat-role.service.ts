c6042807b8dfb979223e229471c64ebdb1b5c0fb
Oleksandr Igumnov <igumssukr@gmail.com>
2024-01-06 19:39:41 +0200

    Created all CRUD operations for ChatRole entity

--- backend/src/chat-role/chat-role.service.ts
+++ backend/src/chat-role/chat-role.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/core/prisma/prisma.service';
+import { ChatRoleEntity } from './entities/chat-role.entity';
+import { CreateChatRoleDto } from './dto/create-chat-role.dto';
+import { UpdateChatRoleDto } from './dto/update-chat-role.dto';
 
 @Injectable()
 export class ChatRoleService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<ChatRoleEntity[]> {
+    return this.prismaService.chatRole.findMany();
+  }
+
+  async create(data: CreateChatRoleDto): Promise<ChatRoleEntity> {
+    return this.prismaService.chatRole.create({ data });
+  }
+
+  async update(name: string, data: UpdateChatRoleDto): Promise<ChatRoleEntity> {
+    return this.prismaService.chatRole.update({ where: { name }, data });
+  }
+
+  async remove(name: string): Promise<ChatRoleEntity> {
+    return this.prismaService.chatRole.delete({ where: { name } });
+  }
 }
