36357f3c15bbf305616f06211f4d25f052ca297d
Oleksandr Igumnov <igumssukr@gmail.com>
2024-01-06 16:22:56 +0200

    Created all CRUD operations for UserReactionType entity

--- backend/src/user-reaction-type/user-reaction-type-service.ts
+++ backend/src/user-reaction-type/user-reaction-type-service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/core/prisma/prisma.service';
+import { UserReactionTypeEntity } from './entities/user-reaction-type.entity';
+import { CreateUserReactionTypeDto } from './dto/create-user-reaction-type.dto';
+import { UpdateUserReactionTypeDto } from './dto/update-user-reaction-type.dto';
 
 @Injectable()
 export class UserReactionTypeService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<UserReactionTypeEntity[]> {
+    return this.prismaService.userReactionType.findMany();
+  }
+
+  async create(data: CreateUserReactionTypeDto): Promise<UserReactionTypeEntity> {
+    return this.prismaService.userReactionType.create({ data });
+  }
+
+  async update(name: string, data: UpdateUserReactionTypeDto): Promise<UserReactionTypeEntity> {
+    return this.prismaService.userReactionType.update({ where: { name }, data });
+  }
+
+  async remove(name: string): Promise<UserReactionTypeEntity> {
+    return this.prismaService.userReactionType.delete({ where: { name } });
+  }
 }
--- backend/src/user-reaction-type/user-reaction-type.controller.ts
+++ backend/src/user-reaction-type/user-reaction-type.controller.ts
@@ -1,7 +1,103 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UserReactionTypeService } from './user-reaction-type-service';
+import {
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { throwHttpExceptionBasedOnErrorType } from 'src/core/error-handling/error-handler';
+import { UserReactionTypeEntity } from './entities/user-reaction-type.entity';
+import { UpdateUserReactionTypeDto } from './dto/update-user-reaction-type.dto';
+import { CreateUserReactionTypeDto } from './dto/create-user-reaction-type.dto';
 
+@ApiTags('User reaction types')
 @Controller('user-reaction-types')
 export class UserReactionTypeController {
   constructor(private readonly userReactionTypeService: UserReactionTypeService) {}
+
+  @ApiCreatedResponse({
+    description: 'User reaction type was successfully created.',
+    type: UserReactionTypeEntity,
+  })
+  @ApiConflictResponse({
+    description: 'Cannot create user reaction type. Invalid data was provided.',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error was occured.',
+  })
+  @Post()
+  create(@Body() createUserReactionTypeDto: CreateUserReactionTypeDto) {
+    return this.userReactionTypeService
+      .create(createUserReactionTypeDto)
+      .then(response => response)
+      .catch(error => throwHttpExceptionBasedOnErrorType(error));
+  }
+
+  @ApiOkResponse({
+    description: 'The list of user reaction types',
+    type: [UserReactionTypeEntity],
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error was occured.',
+  })
+  @Get()
+  findAll() {
+    return this.userReactionTypeService
+      .findAll()
+      .then(response => response)
+      .catch(error => throwHttpExceptionBasedOnErrorType(error));
+  }
+
+  @ApiOkResponse({
+    description: 'User reaction type was successfully updated.',
+    type: UserReactionTypeEntity,
+  })
+  @ApiNotFoundResponse({
+    description: 'The user reaction type with requested name was not found.',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error was occured.',
+  })
+  @Put(':name')
+  @ApiParam({
+    name: 'name',
+    description: 'The name of the user reaction type to be updated',
+    schema: { example: 'Like' },
+  })
+  update(
+    @Param('name') name: string,
+    @Body() updateUserReactionTypeDto: UpdateUserReactionTypeDto,
+  ) {
+    return this.userReactionTypeService
+      .update(name, updateUserReactionTypeDto)
+      .then(response => response)
+      .catch(error => throwHttpExceptionBasedOnErrorType(error));
+  }
+
+  @ApiOkResponse({
+    description: 'User reaction type was successfully removed.',
+    type: UserReactionTypeEntity,
+  })
+  @ApiNotFoundResponse({
+    description: 'The user reaction type with requested name was not found.',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error was occured.',
+  })
+  @Delete(':name')
+  @ApiParam({
+    name: 'name',
+    description: 'The name of the user reaction type to be deleted',
+    schema: { example: 'Like' },
+  })
+  remove(@Param('name') name: string) {
+    return this.userReactionTypeService
+      .remove(name)
+      .then(response => response)
+      .catch(error => throwHttpExceptionBasedOnErrorType(error));
+  }
 }
