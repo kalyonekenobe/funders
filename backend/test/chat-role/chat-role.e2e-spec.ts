51b0889bc812d01051b91eaeb923674a4d58fe3d
Oleksandr Igumnov <igumssukr@gmail.com>
2024-01-08 18:58:06 +0200

    Updated ChatRole tests

--- backend/test/chat-role/chat-role.e2e-spec.ts
+++ backend/test/chat-role/chat-role.e2e-spec.ts
@@ -20,12 +20,12 @@ describe('ChatRoleController (e2e)', () => {
     await app.init();
   });
 
-  it('/chat-roles (GET) --> 200 OK', () => {
+  it('/chats/roles (GET) --> 200 OK', () => {
     MockDataStorage.setDefaultItems();
 
     const initialData = [...MockDataStorage.items()];
     return request(app.getHttpServer())
-      .get('/chat-roles')
+      .get('/chats/roles')
       .expect(HttpStatus.OK)
       .then(response => {
         expect(JSON.stringify(response.body)).toEqual(JSON.stringify(MockDataStorage.items()));
@@ -34,12 +34,12 @@ describe('ChatRoleController (e2e)', () => {
       });
   });
 
-  it('/chat-roles (POST) --> 201 CREATED', () => {
+  it('/chats/roles (POST) --> 201 CREATED', () => {
     MockDataStorage.setDefaultItems();
 
     const initialData = [...MockDataStorage.items()];
     return request(app.getHttpServer())
-      .post('/chat-roles')
+      .post('/chats/roles')
       .send(MockDataStorage.createChatRoleDtoList[0])
       .expect(HttpStatus.CREATED)
       .then(response => {
@@ -54,12 +54,12 @@ describe('ChatRoleController (e2e)', () => {
       });
   });
 
-  it('/chat-roles (POST) --> 409 CONFLICT | Chat role with specified name already exists', () => {
+  it('/chats/roles (POST) --> 409 CONFLICT | Chat role with specified name already exists', () => {
     MockDataStorage.setDefaultItems();
 
     const initialData = [...MockDataStorage.items()];
     return request(app.getHttpServer())
-      .post('/chat-roles')
+      .post('/chats/roles')
       .send(MockDataStorage.items()[0])
       .expect(HttpStatus.CONFLICT)
       .then(() => {
@@ -68,12 +68,12 @@ describe('ChatRoleController (e2e)', () => {
       });
   });
 
-  it('/chat-roles/:name (PUT) --> 200 OK', () => {
+  it('/chats/roles/:name (PUT) --> 200 OK', () => {
     MockDataStorage.setDefaultItems();
 
     const initialData = [...MockDataStorage.items()];
     return request(app.getHttpServer())
-      .put(`/chat-roles/${MockDataStorage.updateChatRoleDtoList[0].name}`)
+      .put(`/chats/roles/${MockDataStorage.updateChatRoleDtoList[0].name}`)
       .send(MockDataStorage.updateChatRoleDtoList[0].data)
       .expect(HttpStatus.OK)
       .then(response => {
@@ -91,12 +91,12 @@ describe('ChatRoleController (e2e)', () => {
       });
   });
 
-  it('/chat-roles/:name (PUT) --> 404 NOT FOUND | Chat role with specified name was not found', () => {
+  it('/chats/roles/:name (PUT) --> 404 NOT FOUND | Chat role with specified name was not found', () => {
     MockDataStorage.setDefaultItems();
 
     const initialData = [...MockDataStorage.items()];
     return request(app.getHttpServer())
-      .put(`/chat-roles/${MockDataStorage.createChatRoleDtoList[0].name}_not_existing_name`)
+      .put(`/chats/roles/${MockDataStorage.createChatRoleDtoList[0].name}_not_existing_name`)
       .send(MockDataStorage.updateChatRoleDtoList[0].data)
       .expect(HttpStatus.NOT_FOUND)
       .then(() => {
@@ -105,12 +105,12 @@ describe('ChatRoleController (e2e)', () => {
       });
   });
 
-  it('/chat-roles/:name (DELETE) --> 200 OK', () => {
+  it('/chats/roles/:name (DELETE) --> 200 OK', () => {
     MockDataStorage.setDefaultItems();
 
     const initialData = [...MockDataStorage.items()];
     return request(app.getHttpServer())
-      .delete(`/chat-roles/${MockDataStorage.removeChatRoleDtoList[1].name}`)
+      .delete(`/chats/roles/${MockDataStorage.removeChatRoleDtoList[1].name}`)
       .expect(HttpStatus.OK)
       .then(response => {
         expect(JSON.stringify(response.body)).toEqual(
@@ -123,12 +123,12 @@ describe('ChatRoleController (e2e)', () => {
       });
   });
 
-  it('/chat-roles/:name (DELETE) --> 404 NOT FOUND | Chat role with specified name was not found', () => {
+  it('/chats/roles/:name (DELETE) --> 404 NOT FOUND | Chat role with specified name was not found', () => {
     MockDataStorage.setDefaultItems();
 
     const initialData = [...MockDataStorage.items()];
     return request(app.getHttpServer())
-      .delete(`/chat-roles/${MockDataStorage.removeChatRoleDtoList[0].name}_not_existing_name`)
+      .delete(`/chats/roles/${MockDataStorage.removeChatRoleDtoList[0].name}_not_existing_name`)
       .expect(HttpStatus.NOT_FOUND)
       .then(() => {
         expect(MockDataStorage.items()).toEqual(initialData);
