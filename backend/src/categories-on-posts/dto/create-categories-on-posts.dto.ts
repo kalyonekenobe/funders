675abbca6e02b04f810afbb705fb001d0ee20193
Oleksandr Igumnov <igumssukr@gmail.com>
2024-01-10 20:17:00 +0200

    Created CategoriesOnPosts entity and dto

---
+++ backend/src/categories-on-posts/dto/create-categories-on-posts.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDefined, IsNotEmpty, IsString, IsUUID, Matches, MaxLength } from 'class-validator';
+import { PostCategoryEntity } from 'src/post-category/entities/post-category.entity';
+import { PostEntity } from 'src/post/entities/post.entity';
+import { CategoriesOnPostsEntity } from '../entities/categories-on-posts.entity';
+
+export class CreateCategoriesOnPostsDto implements CategoriesOnPostsEntity {
+  @ApiProperty({
+    description: 'Post uuid',
+    examples: ['b7af9cd4-5533-4737-862b-78bce985c987', '989d32c2-abd4-43d3-a420-ee175ae16b98'],
+    default: '989d32c2-abd4-43d3-a420-ee175ae16b98',
+  })
+  @IsUUID()
+  @IsNotEmpty()
+  @IsDefined()
+  postId: string;
+
+  @ApiProperty({
+    description: 'Name of the post category',
+    examples: ['Army', 'Talents', 'Poor people', 'Animals'],
+    default: 'Army',
+  })
+  @Matches(/^[a-zA-Z_0-9 ]+$/)
+  @MaxLength(50)
+  @IsString()
+  @IsNotEmpty()
+  @IsDefined()
+  category: string;
+
+  @ApiProperty({ description: 'The nested post object for this CategoriesOnPostsEntity' })
+  post?: PostEntity;
+
+  @ApiProperty({ description: 'The nested postCategory object for this CategoriesOnPostsEntity' })
+  postCategory?: PostCategoryEntity;
+}
