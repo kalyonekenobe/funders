fb11f494a5b851abe5f7cc2cb1b9dc71af62f870
kalyonekenobe <igumssukr@gmail.com>
2024-05-17 12:27:35 +0300

    Updated ssl certificates script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(funders.csfl.eu www.funders.csfl.eu)
+domains=(funders.ehcp.eu www.funders.ehcp.eu)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
